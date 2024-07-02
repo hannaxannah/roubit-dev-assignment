5ac7294c4537276812fc20741b331b1de2fcedb7
Hannah <shannaxannah2@gmail.com>
2024-07-02 16:55:59 +0900

    로그인 UI 수정

--- src/app/components/login/loginForm.tsx
+++ src/app/components/login/loginForm.tsx
@@ -1,17 +1,51 @@
-export default function LoginForm() {
+type LogInFormProps = {
+  formData: {
+    phoneNumberOrEmail: string;
+    password: string;
+  };
+  validationErrors: {
+    phoneNumberOrEmail: string;
+    password: string;
+  };
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function LoginForm(props: LogInFormProps) {
+  const { formData, validationErrors, handleChange } = props;
+
+  const formFields = [
+    {
+      name: "phoneNumberOrEmail",
+      label: "Phone number or Email",
+      type: "text",
+      placeholder: "Phone number or Email",
+      value: formData.phoneNumberOrEmail,
+      error: validationErrors.phoneNumberOrEmail,
+    },
+    {
+      name: "password",
+      label: "Password",
+      type: "password",
+      placeholder: "Password",
+      value: formData.password,
+      error: validationErrors.password,
+    },
+  ];
+
   return (
     <>
       <form className="w-[327px] mt-[28px] flex flex-col m-auto gap-[16px]">
-        <input
-          className="w-full h-[52px] px-[16px] py-[10px] ring-[1px] ring-[#CCCCCE] rounded-[12px] placeholder:font-pretendard font-medium text-[14px] color-[#B6B6B8]"
-          type="text"
-          placeholder="Phone number or Email"
-        />
-        <input
-          className="w-full h-[52px] px-[16px] py-[10px] ring-[1px] ring-[#CCCCCE] rounded-[12px] placeholder:font-pretendard font-medium text-[14px] color-[#B6B6B8]"
-          type="text"
-          placeholder="Password"
-        />
+        {formFields.map((field, index) => (
+          <input
+            className="w-full h-[52px] px-[16px] py-[10px] ring-[1px] ring-[#CCCCCE] rounded-[12px] placeholder:font-pretendard font-medium text-[14px] color-[#B6B6B8] focus:ring-[0.125rem] focus:ring-[#26BD81] focus:outline-none focus:text-[#161617]"
+            type={field.type}
+            placeholder={field.placeholder}
+            name={field.name}
+            value={field.value}
+            onChange={handleChange}
+            key={index}
+          />
+        ))}
       </form>
     </>
   );
