0d0f6dd3dfeef0e1bf30da48fee396362b3d0bf5
Hannah <shannaxannah2@gmail.com>
2024-07-02 15:53:06 +0900

    svg 변경

--- src/app/components/todolist/ToDoItems.tsx
+++ src/app/components/todolist/ToDoItems.tsx
@@ -1,5 +1,7 @@
 import ToDoDelete from "./ToDoDelete";
 import ToDoCheck from "./ToDoCheck";
+import Image from "next/image";
+import line from "../../../../public/Vector 815.svg";
 
 export default function ToDoItems() {
   return (
@@ -14,15 +16,7 @@ export default function ToDoItems() {
         </div>
         {/* line after added */}
         <div className="my-[20px]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="327"
-            height="2"
-            viewBox="0 0 327 2"
-            fill="none"
-          >
-            <path d="M0 1H327" stroke="#F3F3F5" />
-          </svg>
+          <Image src={line} alt={line} width={327} height={2} />
         </div>
         {/* To-Do Item :checked, added */}
         <div className="w-full h-[24px] flex items-center gap-[10px]">
