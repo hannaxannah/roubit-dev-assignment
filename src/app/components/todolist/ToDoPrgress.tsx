54e2175a5f95e999e2809c7cc1c55b52dfff7219
Hannah <shannaxannah2@gmail.com>
2024-07-04 10:53:39 +0900

    진행바 UI 변경

--- src/app/components/todolist/ToDoPrgress.tsx
+++ src/app/components/todolist/ToDoPrgress.tsx
@@ -5,6 +5,9 @@ interface ProgressPros {
 const ToDoProgress = (props: ProgressPros) => {
   const { todos, completedTodos } = props;
 
+  // 진행률 바 길이
+  const progressWidth = todos > 0 ? (completedTodos / todos) * 100 : 0;
+
   return (
     <>
       {/* Tasks Done */}
@@ -12,8 +15,15 @@ const ToDoProgress = (props: ProgressPros) => {
         <div className="font-pretendard font-semibold text-[14px] text-[#26BD81] tracking-tight">
           {completedTodos} of {todos} tasks done
         </div>
+        {/* 진행 바 배경 */}
         <div className="w-full h-[20px] mt-[10px] px-[16px] py-[5px] flex bg-[#26BD81] rounded-[12px]">
-          <div className="w-full h-[4px] bg-[#FFFFFF] bg-opacity-30 rounded-[111px]"></div>
+          <div className="w-full h-[4px] bg-[#FFFFFF] bg-opacity-30 rounded-[111px] relative">
+            {/* 진행률(완료 비율) */}
+            <div
+              className="absolute top-0 left-0 h-full bg-[#FFFFFF] rounded-[111px] transition-[width] duration-300 ease-in-out"
+              style={{ width: `${progressWidth}%`, transform: "translateZ(0)" }}
+            ></div>
+          </div>
         </div>
       </div>
     </>
